e2c6206c9474519a8d186ce615571bf936c8af85
Austasty <obuesiogonna2@gmail.com>
2024-07-21 18:37:32 +0100

    updated home

--- WriteLux-main/users/urls.py
+++ WriteLux-main/users/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('registration/', views.register, name='register'),
+    path('register/', views.register, name='register'),
     path('login/', views.login_user, name='login_user'),
     path('logout/', views.logout_user, name='logout_user'),
 ]
